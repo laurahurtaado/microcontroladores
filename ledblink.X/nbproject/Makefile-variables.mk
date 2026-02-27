3caff88e7835d3f93084dc7f9741bb004fce7d4d
laurahurtaado <lauraihurtado@unicauca.edu.co>
2026-02-27 07:15:11 -0500

    Se agrega proyecto LED Blink con Timer0
    Proyecto en ensamblador para PIC18F4550 usando interrupciones

---
+++ ledblink.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ledblink.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ledblink.X.production.hex
